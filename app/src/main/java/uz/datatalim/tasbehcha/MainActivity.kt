315df1ec3ba6fd420b7b1792c64b26accf540596
mavsumbekdev <sgithacker@gmail.com>
2023-07-22 11:11:22 +0500

    button va text qo'ydim

--- app/src/main/java/uz/datatalim/tasbehcha/MainActivity.kt
+++ app/src/main/java/uz/datatalim/tasbehcha/MainActivity.kt
@@ -2,10 +2,21 @@ package uz.datatalim.tasbehcha
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    var sanoq = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun count(view: View) {
+        var btn = findViewById<Button>(R.id.count)
+        btn.setOnClickListener {
+            sanoq++
+            btn.setText(sanoq.toString())
+        }
     }
 }
