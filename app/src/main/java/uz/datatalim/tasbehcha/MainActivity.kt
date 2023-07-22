3cd5ddef4e2c759fe6fb8b3d828212896222b49e
mavsumbekdev <sgithacker@gmail.com>
2023-07-22 11:27:44 +0500

    counter qo'shdim

--- app/src/main/java/uz/datatalim/tasbehcha/MainActivity.kt
+++ app/src/main/java/uz/datatalim/tasbehcha/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     var sanoq = 0
@@ -14,6 +15,11 @@ class MainActivity : AppCompatActivity() {
 
     fun count(view: View) {
         var btn = findViewById<Button>(R.id.count)
+        var text = findViewById<TextView>(R.id.text)
+        text.setOnClickListener{
+            sanoq++
+            text.setText(sanoq.toString())
+        }
         btn.setOnClickListener {
             sanoq++
             btn.setText(sanoq.toString())
