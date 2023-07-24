9b1bf7c9cbf3b98c38f12beed1c7606782734532
mavsumbekdev <sgithacker@gmail.com>
2023-07-24 10:26:31 +0500

    dastur to'liq yasaldi

--- app/src/main/java/uz/datatalim/tasbehcha/MainActivity.kt
+++ app/src/main/java/uz/datatalim/tasbehcha/MainActivity.kt
@@ -9,22 +9,27 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     var sanoq = 0
+    var jami=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var btn = findViewById<FloatingActionButton>(R.id.count)
         var text = findViewById<TextView>(R.id.text)
+        var tvJami = findViewById<TextView>(R.id.jami)
         var reset = findViewById<FloatingActionButton>(R.id.reset)
         btn.setOnClickListener {
             sanoq++
+            jami++
+            tvJami.text="Jami : $jami"
             if (sanoq > 33) {
                 sanoq=0
             }
-            text.setText(sanoq.toString())
             text.text="$sanoq/33"
         }
         reset.setOnClickListener {
             sanoq=0
+            jami=0
+            tvJami.text="Jami : $jami"
             text.text="$sanoq/33"
         }
     }
